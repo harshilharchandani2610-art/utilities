a9d60ceba8b4460e1ac70e7fb4c2108692c74ef9
harshilharchandani2610-art <harshilharchandani2610@gmail.com>
2026-03-25 12:03:07 +0530

    Update FINDING THE NUMBER THAT DIDN'T APPEARS ONCE.cpp

--- FINDING THE NUMBER THAT DIDN'T APPEARS ONCE.cpp
+++ FINDING THE NUMBER THAT DIDN'T APPEARS ONCE.cpp
@@ -6,16 +6,18 @@ int main() {
     cout << "Enter the number of elements in an Array: ";
     cin >> n;
     int arr[100];
+    // input of an Array
     for(int i = 0; i < n; i++) {
         cout << "Enter the element " << i + 1 << ": ";
         cin >> arr[i];
     }
-    
+    // output of a given Array 
     cout << "\nYour given Array is: ";
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
+    // loop to check wheather the number appears once or NOT!!
     for(int i = 0; i < n; i++) {
         int num = arr[i];
         int count = 0;  
