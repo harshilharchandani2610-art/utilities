b1b9cea9d20fa166d7ddc02578a1e58dcdce7fe1
harshilharchandani2610-art <harshilharchandani2610@gmail.com>
2026-03-25 11:58:13 +0530

    Refactor CONSCEUTIVE ONES.cpp with comments
    Added comments for clarity and improved code readability.

--- CONSCEUTIVE ONES.cpp
+++ CONSCEUTIVE ONES.cpp
@@ -5,10 +5,12 @@ int main() {
     cout<<"enter the number of elements in an Array : ";
     cin>>n;
     int arr[100];
+    // input of an Array
     for(int i=0;i<n;i++){
         cout<<"enter the element "<<i+1<<" :";
         cin>>arr[i];
     }
+    // output of given an Array
     cout<<"\n your given Array is."<<endl;
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
@@ -16,6 +18,8 @@ int main() {
     cout<<endl;
     int maxi=0;
     int count=0;
+    // counts the number of conscetive ones in the given Array 
+    // for exapmle A[] = [2,1,1,1,2,2,3,1,]
     for(int i=0;i<n;i++){
         if(arr[i]==1){
             count ++;
@@ -26,4 +30,5 @@ int main() {
         }
     }
     cout<<"Maximum conscetive ones in this Array is : "<<maxi;
+    // maximum number of ones will be 3 
 }
